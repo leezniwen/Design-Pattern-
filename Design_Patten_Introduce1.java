a815d55b994ed534475cb38d06854ceb00c37c5b
leezniwen <msn1250@gmail.com>
2020-08-30 12:03:00 +0800

    Introduce

---
+++ Design_Patten_Introduce1.java
@@ -0,0 +1,17 @@
+import java.lang.ProcessBuilder.Redirect.Type;
+
+public class Design_Patten_Introduce1 {
+    public class Animal {
+        public Animal(Animal animal) {
+        Animal Type = animal; 
+        
+    }
+        public String  makesound(){
+            
+            return "Nothing";
+        }
+    }     
+    public static void main (String args[]){
+
+    }
+}
